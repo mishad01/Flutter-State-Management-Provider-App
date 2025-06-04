b7c6c6e8fa8f0a3c7043236ed6bf5d9663204ac9
SAKIF RAHMAN MISHAD <109865291+mishad01@users.noreply.github.com>
2025-06-04 23:18:44 +0600

    chore: Implement basic transaction model and provider

---
+++ expense_tracker_app/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:expense_tracker_app/provider/transaction_provider.dart';
+import 'package:expense_tracker_app/view/home_screen_way2.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => TransactionProvider(),
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomeScreenWay2(),
+    );
+  }
+}
